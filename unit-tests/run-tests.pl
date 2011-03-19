f86f7052663258161c4b9db4f10678c4216a9b84
David Bremner <bremner@unb.ca>
2011-03-19 08:22:35 -0300

    Add gpg key generation from rfc822 template file.
    This is pretty much translated from tests/basic.  It is unfortunately
    a bit more verbose in perl. It could be made shorter by avoiding
    GnuPG::Interface.

--- unit-tests/run-tests.pl
+++ unit-tests/run-tests.pl
@@ -4,8 +4,38 @@ use strict;
 use TAP::Harness;
 use File::Find;
 use FindBin;
+use GnuPG::Interface;
+use GnuPG::Handles;
+use File::Temp qw(tempdir);
+
 my $BINDIR;
 BEGIN { $BINDIR = $FindBin::Bin; }
+
+
+{ 
+# Generate Keys from template file
+
+  my $tempdir = tempdir("/tmp/test-gnupgXXXXX", CLEANUP=> 1);
+  my $gnupg = new GnuPG::Interface();
+  $gnupg->options->hash_init(homedir=>$tempdir,batch=>1);
+
+  my $GPGQR='';
+  if (system qw(gpg --quick-random --version) ==0) {
+    $GPGQR='--quick-random';
+  } elsif (system qw(gpg --debug-quick-random --version) ==0) {
+    $GPGQR='--debug-quick-random';
+  }
+
+  print STDERR "WARNING: no quick random option found. Tests may hang!\n" 
+    unless(scalar $GPGQR);
+
+  my $pid = $gnupg->wrap_call( commands=>[qw(--gen-key --batch),$GPGQR],
+			       command_args=>[$BINDIR.'/keys.txt'],
+			       handles=>new GnuPG::Handles() );
+  waitpid $pid,0;
+
+  $ENV{GNUPGHOME}=$tempdir;
+}
 
 my @dirs = scalar(@ARGV) > 0 ? @ARGV : ($BINDIR);
 
